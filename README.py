23924b9dfa06d83ac0dd5c866b631e0a14b3e642
mahishah2402-source <mahishah2402@gmail.com>
2026-05-09 22:46:35 +0530

    Refactor expense form submission logic

--- README.py
+++ README.py
@@ -1,22 +1,13 @@
 # Expense-Calc
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-
 st.title("Family Expense Tracker")
-
-# Connect to Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read()
-
-# Input Form
 with st.form("expense_form"):
     category = st.selectbox("Category", ["Food", "Utilities", "Entertainment"])
     amount = st.number_input("Amount", min_value=0.0)
-    submit = st.form_submit_button("Add Expense")
-
-   if submit:
-          # Logic to add new row to Google Sheets
+    submit = st.form_submit_button("Add Expense") 
+                              if submit:
           new_data = {"Category": category, "Amount": amount}
-          # (Append logic goes here)
           st.success("Expense added!")
-          
