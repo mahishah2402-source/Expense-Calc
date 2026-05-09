c6c1139f0235c94a76c70e82c9119c983d9c2cd9
mahishah2402-source <mahishah2402@gmail.com>
2026-05-09 23:40:17 +0530

    Change amount input min_value from 0 to 0.0

--- exptrack.py
+++ exptrack.py
@@ -18,7 +18,7 @@ df = conn.read(ttl="0s") # ttl="0s" ensures it fetches fresh data every time
 with st.form(key="expense_form"):
     date = st.date_input("Date")
     category = st.selectbox("Category", ["Food", "Utilities", "Entertainment", "Other"])
-    amount = st.number_input("Amount", min_value=0, step=0.01)
+    amount = st.number_input("Amount", min_value=0.0, step=0.01)
     note = st.text_input("Note (Optional)")
     
     submit_button = st.form_submit_button(label="Add Expense")
